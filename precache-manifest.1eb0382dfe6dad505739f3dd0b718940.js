5f2bb7699d29d10d3c5699c719f77c535d29ab1c
oksanaVlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2019-11-21 16:45:42 +0200

    Updates

---
+++ precache-manifest.1eb0382dfe6dad505739f3dd0b718940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da949ede2dde76c4193f82228ac3c732",
+    "url": "/hotel-project/index.html"
+  },
+  {
+    "revision": "ee73110685d9e7be33c7",
+    "url": "/hotel-project/static/css/2.b39d42bf.chunk.css"
+  },
+  {
+    "revision": "74694055e9eefc2535c2",
+    "url": "/hotel-project/static/css/main.b3f91041.chunk.css"
+  },
+  {
+    "revision": "ee73110685d9e7be33c7",
+    "url": "/hotel-project/static/js/2.aef8b866.chunk.js"
+  },
+  {
+    "revision": "74694055e9eefc2535c2",
+    "url": "/hotel-project/static/js/main.672614c6.chunk.js"
+  },
+  {
+    "revision": "99455ac7f4abf9689b18",
+    "url": "/hotel-project/static/js/runtime-main.67115f3b.js"
+  }
+]);
